1e466f6ff2451a90b2005aadb62cadf67450caf5
Angga Widiarta <anggawidiarta55@gmail.com>
2024-02-27 00:47:59 +0800

    chore: modifying style

--- src/components/AnimatedPinDemo.tsx
+++ src/components/AnimatedPinDemo.tsx
@@ -1,24 +1,46 @@
+// AnimatedPinDemo.tsx
+
 "use client";
 import React from "react";
 import { PinContainer } from "./ui/3d-pin";
 
-export function AnimatedPinDemo() {
+/**
+ * Props for the AnimatedPinDemo component
+ */
+interface AnimatedPinDemoProps {
+  title: string; // The title of the pin
+  desc: string; // The description of the pin
+  href: string; // The URL the pin should navigate to
+  backgroundImageUrl: string; // The URL for the background image
+}
+
+/**
+ * Component for displaying an animated pin with title, description, and background image
+ * @param title - The title of the pin
+ * @param desc - The description of the pin
+ * @param href - The URL the pin should navigate to
+ * @param backgroundImageUrl - The URL for the background image
+ */
+export function AnimatedPinDemo({
+  title,
+  desc,
+  href,
+  backgroundImageUrl,
+}: AnimatedPinDemoProps) {
   return (
     <div className="h-[40rem] w-full flex items-center justify-center ">
-      <PinContainer title="/ui.aceternity.com" href="google.com">
+      <PinContainer title={title} href={href}>
         <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[24rem] h-[30rem] ">
-          <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-            Aceternity UI
+          <h3 className="max-w-xs !pb-2 !m-0 font-bold text-base lg:text-xl text-slate-100 w-max">
+            {title}
           </h3>
-          <div className="text-base !m-0 !p-0 font-normal">
-            <span className="text-slate-300 ">
-              Customizable Tailwind CSS and Framer Motion Components.
-            </span>
+          <div className="lg:text-lg text-base !m-0 !p-0 font-normal w-max">
+            <span className="text-slate-500 ">{desc}</span>
           </div>
           <div
-            className="flex flex-1 w-full rounded-lg mt-4 bg-cover bg-center"
+            className="flex flex-1 w-full mt-4 bg-center bg-cover rounded-lg"
             style={{
-              backgroundImage: 'url("src/images/martin-katler-WTN4tbN_tNQ-unsplash.jpg")',
+              backgroundImage: `url(${backgroundImageUrl})`,
             }}
           />
         </div>
--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,6 +1,11 @@
+// Import the ClassValue type and clsx function from the clsx library
 import { type ClassValue, clsx } from "clsx";
+
+// Import the twMerge function from the tailwind-merge library
 import { twMerge } from "tailwind-merge";
 
+// Define a function called cn that takes in a variable number of inputs of type ClassValue
 export function cn(...inputs: ClassValue[]) {
+  // Merge the inputs using the clsx function and then merge the result using the twMerge function
   return twMerge(clsx(inputs));
 }
