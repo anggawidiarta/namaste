16fcf37e2cb100a1111d9c1cf647dfa7fa4f663a
Angga Widiarta <anggawidiarta55@gmail.com>
2024-02-27 00:11:05 +0800

    chore: changing container size

--- src/components/AnimatedPinDemo.tsx
+++ src/components/AnimatedPinDemo.tsx
@@ -5,11 +5,8 @@ import { PinContainer } from "./ui/3d-pin";
 export function AnimatedPinDemo() {
   return (
     <div className="h-[40rem] w-full flex items-center justify-center ">
-      <PinContainer
-        title="/ui.aceternity.com"
-        href="google.com"
-      >
-        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[22rem] h-[26rem] ">
+      <PinContainer title="/ui.aceternity.com" href="google.com">
+        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[24rem] h-[30rem] ">
           <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
             Aceternity UI
           </h3>
@@ -18,7 +15,12 @@ export function AnimatedPinDemo() {
               Customizable Tailwind CSS and Framer Motion Components.
             </span>
           </div>
-          <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+          <div
+            className="flex flex-1 w-full rounded-lg mt-4 bg-cover bg-center"
+            style={{
+              backgroundImage: 'url("src/images/martin-katler-WTN4tbN_tNQ-unsplash.jpg")',
+            }}
+          />
         </div>
       </PinContainer>
     </div>
