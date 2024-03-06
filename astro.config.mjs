27eb7cc84906fe11d5f8038271a5a565a87e0977
Angga Widiarta <anggawidiarta55@gmail.com>
2024-03-06 23:22:09 +0800

    normal update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'static',
+  output: "static",
 });
